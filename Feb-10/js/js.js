f9fd079e5924e324fd099716a72c08c2651a8e0c
AllisonGriff <gallie8699@gmail.com>
2026-02-10 17:54:32 -0500

    adding notes-- will transfer to  markdown file

---
+++ Feb-10/js/js.js
@@ -0,0 +1,25 @@
+// Objects in JS feel like Dict in python
+
+let examplePerson = {
+    name : "John",
+    age : 30, 
+    email : "John25@gmail.com",
+}
+
+// examplePerson.age  <--- dot notation
+                        //the same thing... key value look up
+// examplePerson['age'] <-- look up or bracket notation
+
+// some gothcas 
+/*
+when we iterate throug these objects.
+
+let someExample = [ 2, 3, 4];
+
+for( let e of someExample){
+console.log (e)}
+
+
+-- Everything is unknown until it is not unknown
+*/
+
