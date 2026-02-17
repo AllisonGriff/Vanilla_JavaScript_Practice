4396910b08d9a176d093071de54ede3f6fc0564b
AllisonGriff <gallie8699@gmail.com>
2026-02-17 18:29:50 -0500

    finishing up

--- feb-17/js/js.js
+++ feb-17/js/js.js
@@ -2,22 +2,77 @@
 
 // Callback function
 
-function logAMessage(event){
-    console.log(event);
-    console.log("Did Something")
+// function logAMessage(event){
+//     console.log(event);
+//     console.log("Did Something")
+// }
+
+// let arrowButton = document.querySelector('#function');
+
+// arrowButton.addEventListener('click', logAMessage);
+
+
+
+// // arrow functions?
+// document.querySelector('#arrow').addEventListener(
+//     'click', 
+//     (event) => {
+//         console.log(event.clientX);
+
+//     }
+// )
+
+
+// JS example from the chapter
+// Arrow function == shorter, inline version
+let buttonArrow = document.querySelector('#arrow');
+
+buttonArrow.addEventListener('click', (event) => {
+    alert('Ouch!');
+    console.log("EVENT", event);
+    console.log("EVENT TARGET", event.target);
+    console.log("THIS", this);
+});
+
+// Function declaration - standard, can reuse
+function stopThat(event) {
+    console.log("EVENT", event);
+    console.log("EVENT TARGET", event.target);
+    console.log("THIS", this);
+    alert('Owwww! Stop that!');
 }
 
-let arrowButton = document.querySelector('#function');
-
-arrowButton.addEventListener('click', logAMessage);
 
 
 
-// arrow functions?
-document.querySelector('#arrow').addEventListener(
-    'click', 
-    (event) => {
-        console.log(event.clientX);
 
-    }
+//  declare a function?
+
+function chooseRandomNum(){
+    return 1 + Math.ceil(Math.random() * 100);
+}
+
+
+// event listener
+
+document.querySelector('#function').addEventListener(
+    'click', chooseRandomNum
 )
+
+
+
+
+// Greet the 
+let UserName = document.querySelector('#username')
+
+
+function clearInputDisplayName(event){
+    let name= UserName.value;
+
+    document.querySelector('#greetingElement').textContent = `Hello, ${name}`
+    console.log(name)
+
+}
+
+document.querySelector('#go').addEventListener('click', clearInputDisplayNames)
+// replace ____ with Name
