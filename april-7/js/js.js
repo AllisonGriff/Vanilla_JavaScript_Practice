29fcbe103e0a028f719b24b220e276a61552ee2d
AllisonGriff <gallie8699@gmail.com>
2026-04-07 18:37:23 -0400

    feat: add name input feedback and validation to petstore form

--- april-7/js/js.js
+++ april-7/js/js.js
@@ -6,7 +6,20 @@ function onSubmit(event) {
     // the prevent default is keeping the page from reloading. 
     event.preventDefault();
 
-    console.log(document.forms.petstore.pets.value);
+
+     if (!document.forms.petstore.name.value) {
+
+         document.querySelector("#name").classList.add("error");
+            // name is not present provvide feedback to user
+            document.querySelector(".feedback").textContent = "Please enter your name.";
+           
+
+        //name is not present
+     }else {
+            // name is present
+            console.log(document.forms.petstore.pets.value);
+     }
+
     // your JS code goes here
 }
 
