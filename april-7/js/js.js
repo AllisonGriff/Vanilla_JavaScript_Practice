11993b0724d1aeb5a2d16cc4a288cc372c13c631
AllisonGriff <gallie8699@gmail.com>
2026-04-07 18:11:25 -0400

    feat: update form structure and enhance submission handling in petstore form

--- april-7/js/js.js
+++ april-7/js/js.js
@@ -3,9 +3,10 @@ function onSubmit(event) {
 
     // What does this mean? What is the "default" validation approach?
     //  if not here does the page reload?
+    // the prevent default is keeping the page from reloading. 
     event.preventDefault();
 
-    console.log("clicked submit");
+    console.log(document.forms.petstore);
     // your JS code goes here
 }
 
